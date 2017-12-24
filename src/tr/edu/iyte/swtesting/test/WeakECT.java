b81999338a8c8a71873e5ef0f967206de65de60f
pelinsenkula <pelinsenkula@hotmail.com>
2017-12-24 19:17:42 +0300

    WeakECT eklendi

--- src/tr/edu/iyte/swtesting/test/WeakECT.java
+++ src/tr/edu/iyte/swtesting/test/WeakECT.java
@@ -23,67 +23,36 @@ public class WeakECT {
 		this.inputVariablesList = inputVariablesList;
 	}
 
-	public List<String> generateWeakECTestCases() {
+	public List<Map<String, String>> generateWeakECTestCases() {
 		List<Integer> sizeList = new ArrayList<>();
-		List<String> testCaseKeys = new ArrayList<>();
-		for(int i=0; i<inputVariablesList.size();i++) {
+		List<Map<String, String>> testCases = new ArrayList<>();
+
+		for (int i = 0; i < inputVariablesList.size(); i++) {
 			sizeList.add(inputVariablesList.get(i).getNamesForECs().size());
 		}
 		int maxSize = Collections.max(sizeList);
-		for (int k=0; k<maxSize; k++) {
-			for(int j=0; j<inputVariablesList.size(); j++) {
-				String a = inputVariablesList.get(j).getNamesForECs().get((k%inputVariablesList.get(j).getNamesForECs().size()));				
-				testCaseKeys.add(a);
-			}		
+		for (int k = 0; k < maxSize; k++) {
+			Map<String, String> testCase = new HashMap<>();
+			for (int j = 0; j < inputVariablesList.size(); j++) {
+				String key = inputVariablesList.get(j).getNamesForECs()
+						.get((k % inputVariablesList.get(j).getNamesForECs().size()));
+				testCase.put(inputVariablesList.get(j).getId(), inputVariablesList.get(j).getValuesForECs().get(key));
+			}
+			testCases.add(testCase);
 		}
-		return testCaseKeys;
+
+		return testCases;
 
 	}
-	public static void main(String[] args) throws FileNotFoundException, IOException, InvalidInputException {	
-//		Map<String, String> v1 = new HashMap<>();
-//		List<String> v1Names = new ArrayList<>();
-//		v1.put("m1", "5");
-//		v1Names.add("m1");
-//		v1.put("m2", "2");
-//		v1Names.add("m2");
-//		v1.put("m3", "2");
-//		v1Names.add("m3");
-//		
-//		Map<String, String> v2 = new HashMap<>();
-//		List<String> v2Names = new ArrayList<>();
-//		v2.put("D1", "5");
-//		v2Names.add("D1");
-//		v2.put("D2", "2");
-//		v2Names.add("D2");
-//		v2.put("D3", "3");
-//		v2Names.add("D3");
-//		v2.put("D4", "4 ");
-//		v2Names.add("D4");
-//		
-//		Map<String, String> v3 = new HashMap<>();
-//		List<String> v3Names = new ArrayList<>();
-//		v3.put("y1", "5");
-//		v3Names.add("y1");
-//		v3.put("y2", "3");
-//		v3Names.add("y2");
-//		v3.put("y3", "2");
-//		v3Names.add("y3");
-//		
-//		InputVariables iv = new InputVariables("a", "a","Integer", v1 ,v1Names, "0","1","2","100","199","200","201");
-//		InputVariables iv2 = new InputVariables("b", "b","Integer",v2,v2Names,"0", "1","2","100","199","200","201");
-//		InputVariables iv3 = new InputVariables("c", "c","Integer",v3,v3Names,"0", "1","2","100","199","200","201");
-//		
-//		List<InputVariables> inputVariablesList = new ArrayList<InputVariables>();
-//		inputVariablesList.add(iv);
-//		inputVariablesList.add(iv2);
-//		inputVariablesList.add(iv3);
-//		
+
+	public static void main(String[] args) throws FileNotFoundException, IOException, InvalidInputException {
+
 //		ExcelManager excelManager = new ExcelManager(new FileInputStream("resource\\triangleInput.xlsx"));
 		ExcelManager excelManager = new ExcelManager(new FileInputStream("resource\\dateInput.xlsx"));
 		List<InputVariables> inputVariablesList = excelManager.readInputVariables();
-		
-		WeakECT wect = new WeakECT(inputVariablesList); 
+
+		WeakECT wect = new WeakECT(inputVariablesList);
 		System.out.println(wect.generateWeakECTestCases());
 	}
-	
+
 }
