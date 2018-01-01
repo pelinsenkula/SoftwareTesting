226d74ca19d5a903a4f4f5226aca6ce17b8bb489
emreirtem <emre_irtem@hotmail.com>
2018-01-01 17:05:42 +0300

    c

--- src/tr/edu/iyte/swtesting/model/TestCase.java
+++ src/tr/edu/iyte/swtesting/model/TestCase.java
@@ -1,5 +1,89 @@
 package tr.edu.iyte.swtesting.model;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class TestCase {
+
+	private String testCaseNo;
+	private List<InputVariable> inputValues = new ArrayList<>();
+	private List<OutputPairValues> outputValues = new ArrayList<>();
+
+	public TestCase(String testCaseNo) {
+		setTestCaseNo(testCaseNo);
+	}
+
+	private void setTestCaseNo(String testCaseNo) {
+		this.testCaseNo = testCaseNo;
+	}
+
+	public String getTestCaseNo() {
+		return testCaseNo;
+	}
 	
+	public void addInputValue(String variableId, String value) {
+		inputValues.add(new InputVariable(variableId, value));
+	}
+	
+	public void addOutputValue(String expected, String observed) {
+		outputValues.add(new OutputPairValues(expected, observed));
+	}
+
+	private class OutputPairValues {
+		private String expected;
+		private String observed;
+
+		private OutputPairValues(String expected, String observed) {
+			setExpected(expected);
+			setObserved(observed);
+		}
+
+		private String getExpected() {
+			return expected;
+		}
+
+		private void setExpected(String expected) {
+			this.expected = expected;
+		}
+
+		private String getObserved() {
+			return observed;
+		}
+
+		private void setObserved(String observed) {
+			this.observed = observed;
+		}
+		
+		private Boolean isTestDone() {
+			return false;
+		}
+	}
+
+	private class InputVariable {
+		private String variableId;
+		private String value;
+
+		private InputVariable(String variableId, String value) {
+			setVariableId(variableId);
+			setValue(value);
+		}
+
+		private String getVariableId() {
+			return variableId;
+		}
+
+		private void setVariableId(String variableId) {
+			this.variableId = variableId;
+		}
+
+		private String getValue() {
+			return value;
+		}
+
+		private void setValue(String value) {
+			this.value = value;
+		}
+	}
 }
